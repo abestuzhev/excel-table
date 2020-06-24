88909c402f6f4a5ba86ecd95388caebd12abcf03
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2020-06-24 23:20:10 +0300

    add class Dom

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,26 +1,30 @@
 import {ExcelComponent} from "@core/ExcelComponent";
+import { $ } from "../../core/dom";
 
 export class Excel {
    constructor(selector, options) {
-      this.$el = document.querySelector(selector);
+      this.$el = $(selector);
       this.components = options.components || [];
    }
 
    getRoot(){
-      const $root = document.createElement('div');
-      $root.classList.add('excel');
+      const $root = $.create('div', 'excel');
 
       this.components.map( Component => {
 
-         const $el = document.createElement('div');
-         $el.classList.add(Component.className);
-         const component = new Component();
-
-         $el.innerHTML = component.toHTML();
+         const $el = $.create('div', Component.className);         
+         const component = new Component($el);
+         // console.log($el.$el);
+         // debugger
+         $el.html(component.toHTML());
          $root.append($el);
       });
+      
+      console.log($root);
+      
       return $root;
    }
+
 
    render(){
       this.$el.append(this.getRoot());
---
+++ src/core/dom.js
@@ -0,0 +1,48 @@
+class Dom {
+    constructor(selector){
+        this.$el = typeof selector === "string"
+        ? document.querySelector(selector)
+        : selector
+    }
+
+    html(html) {
+        if(typeof html === "string"){
+            this.$el.innerHTML = html;
+            return this;
+        }
+        return this.$el.outerHTML.trim();
+    }
+
+    clear() {
+        this.$el.html("");
+        return this;
+    }
+
+    append(node){
+        if(node instanceof Dom) {
+            node = node.$el
+        }
+        if(Element.prototype.append) {
+            this.$el.append(node);
+        }else {
+            this.$el.appendChild(node);
+        }
+
+        return this;
+        
+    }
+}
+
+// $("div").html("<h1>Test</h1>").clear();
+
+export function $(selector){
+    return new Dom(selector);
+}
+
+$.create = (tagname, classes = '') => {
+    const el = document.createElement(tagname);
+    if(classes) {
+        el.classList.add(classes);
+    }
+    return $(el);
+}
